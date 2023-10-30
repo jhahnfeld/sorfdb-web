57e9e362082b03707748986788b2300245953ca6
jhahnfel <julian.hahnfeld@computational.bio.uni-giessen.de>
2023-10-30 22:18:19 +0100

    Add not found view

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <div class="container-fluid">
+      <div class="alert alert-danger">
+        <h5>Page not found: {{ $route.fullPath }}</h5>
+      </div>
+    </div>
+  </div>
+</template>
+  
+<script lang="ts">
+export default {
+  name: "Lost",
+};
+</script>
