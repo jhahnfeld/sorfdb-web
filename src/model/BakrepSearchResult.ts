ca4ba55eace600f23aeab9e9fdefddb38c9499e0
jhahnfel <julian.hahnfeld@computational.bio.uni-giessen.de>
2023-10-30 22:12:07 +0100

    Add initial zod schemes for sorfdb

--- src/model/BakrepSearchResult.ts
+++ src/model/BakrepSearchResult.ts
@@ -3,6 +3,43 @@ import { GenomeSchema, StatsSchema } from "./BaktaResults";
 import { QualitySchema } from "./CheckmResults";
 import { ClassfificationSchema } from "./GtdbtkResult";
 
+const PfamEntryScheme = z.object({
+  name: z.string(),
+  evalue: z.number()
+})
+
+const SorfdbEntrySchema = z.object({
+  id: z.string(),
+  source: z.string(), 
+  assembly: z.optional(z.string()),
+  accession: z.optional(z.string()),
+  'protein-id': z.optional(z.string()),
+  uid: z.optional(z.string()),
+  'entry-name': z.optional(z.string()),
+  phylum: z.optional(z.string()),
+  class: z.optional(z.string()),
+  order: z.optional(z.string()),
+  family: z.optional(z.string()),
+  genus: z.optional(z.string()),
+  species: z.optional(z.string()),
+  strain: z.optional(z.string()),
+  sorf: z.optional(z.string()),
+  slen: z.number(),
+  'start-codon': z.optional(z.string()),
+  protein: z.string(),
+  plen: z.number(),
+  product: z.optional(z.string()),
+  rbs: z.nullable(z.number()),
+  'pfam-subject': z.nullable(z.array(PfamEntryScheme)),
+  gravy: z.number(),
+  aromaticity: z.number(),
+  'molecular-weight': z.number(),
+  instability: z.number(),
+  'isoelectric-point': z.number(),
+  'aliphatic-index': z.number(),
+  boman: z.number(),
+});
+
 const SimpleFeatureSchema = z.object({
   gene: z.optional(z.nullable(z.string())),
   product: z.optional(z.nullable(z.string())),
@@ -44,4 +81,6 @@ const BakrepSearchResultSchema = z.object({
 export type BakrepSearchResultEntry = z.infer<typeof BakrepSearchEntrySchema>;
 export type BakrepSearchResult = z.infer<typeof BakrepSearchResultSchema>;
 
-export { BakrepSearchEntrySchema, BakrepSearchResultSchema };
+export type SorfdbEntry = z.infer<typeof SorfdbEntrySchema>;
+
+export { SorfdbEntrySchema, BakrepSearchEntrySchema, BakrepSearchResultSchema };
