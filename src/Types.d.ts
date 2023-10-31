d4d675f3b1ab83f0466f53a44854cc1ab2031c8c
jhahnfel <julian.hahnfeld@computational.bio.uni-giessen.de>
2023-10-31 10:57:21 +0100

    Delete IGV components

--- src/Types.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "igv" {
-  function createBrowser(element: HTMLElement, config: any): Promise<IGVBrowser>;
-  function removeBrowser(element: IGVBrowser): void;
-
-  interface IGVBrowser {
-    visibilityChange();
-  }
-}
