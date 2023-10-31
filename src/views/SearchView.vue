c467440df458e7a51f83bc877951e33d51b76973
jhahnfel <julian.hahnfeld@computational.bio.uni-giessen.de>
2023-10-31 09:18:16 +0100

    Replace bakrep browse field with sorfdb search field

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -12,8 +12,13 @@ import {
 import Pagination from "@/components/pagination/Pagination.vue";
 import type { BakrepSearchResultEntry } from "@/model/BakrepSearchResult";
 import type { SortOption, SortDirection } from "@/model/Search";
+import ExportProgress from "./browse/ExportProgress.vue";
+import { downloadFullTsv, type ProgressEvent } from "./browse/ExportTsv";
 import ResultTable from "@/views/browse/ResultTable.vue";
-import { computed, onMounted, ref, type Ref } from "vue";
+import { computed, onMounted, ref, watch, type Ref } from "vue";
+import Notification from "../components/Notification.vue";
+import { guessInputType, validateProtein, validateDNA, matchesIdScheme } from "../search-validator";
+
 const searchState = usePageState();
 const entries: Ref<BakrepSearchResultEntry[]> = ref([]);
 const api = useApi();
@@ -29,6 +34,90 @@ const gcTuple = ref<FilterTuple>({ from: 0, to: 100 });
 const contigTuple = ref<FilterTuple>({ from: 0, to: 1000 });
 const qualityTuple = ref<FilterTuple>({ from: 0, to: 100 });
 const contaminationTuple = ref<FilterTuple>({ from: 0, to: 100 });
+
+const error = ref('');
+const sequence = ref('');
+const sequenceLength = ref(sequence.value.length)
+const inputType = ref('auto');
+const searchType = ref('exact');
+const identity = ref(90);
+const coverage = ref(80);
+const submitting = ref(false);
+const seqValid = ref(false);
+const valid = ref(false);
+
+watch(sequence, (newSequence) => {
+  sequenceLength.value = newSequence.length
+  if (newSequence.length == 0) {
+    seqValid.value = false
+    error.value = ''
+  }
+  else if (inputType.value == 'auto') {
+    let guessedType = guessInputType(newSequence)
+    if (guessedType.valid) {
+      inputType.value = guessedType.type
+      seqValid.value = true
+      error.value = ''
+    } else {
+      seqValid.value = false
+      error.value = "Could not guess input type."
+    }
+  } else if (inputType.value == 'protein' && validateProtein(newSequence)) {
+    seqValid.value = true
+    error.value = ''
+  } else if (inputType.value == 'dna' && validateDNA(newSequence)) {
+    seqValid.value = true
+    error.value = ''
+  } else if (inputType.value == 'id' && matchesIdScheme(newSequence)) {
+    seqValid.value = true
+    error.value = ''
+  } else {
+    seqValid.value = false
+    error.value = `Could not match input with type: ${inputType.value}`
+  }
+});
+watch(inputType, (newInputType) => {
+        if (sequence.value.length == 0) {
+          seqValid.value = false
+          error.value = ''
+        }
+        else if (newInputType == 'auto') {
+          let guessedType = guessInputType(sequence.value)
+          if (guessedType.valid) {
+            inputType.value = guessedType.type
+            seqValid.value = true
+            error.value = ''
+          } else {
+            seqValid.value = false
+            error.value = "Could not guess input type."
+          }
+        } else if (newInputType == 'protein' && validateProtein(sequence.value)) {
+          seqValid.value = true
+          error.value = ''
+        } else if (newInputType == 'dna' && validateDNA(sequence.value)) {
+          seqValid.value = true
+          error.value = ''
+        } else if (newInputType == 'id' && matchesIdScheme(sequence.value)) {
+          seqValid.value = true
+          error.value = ''
+        } else {
+          seqValid.value = false
+          error.value = `Could not match input with type: ${inputType.value}`
+        }
+      });
+      watch(seqValid, (v) => {
+        if (v) {
+          valid.value = true
+          error.value = ''
+        } else {
+          valid.value = false
+        }
+      });
+      const submit = ()=>{
+        submitting.value = true;
+        console.log(sequence.value, inputType.value);
+        console.log(sequence.value);
+      };
 
 function filter(offset = 0) {
   let query;
@@ -116,22 +205,120 @@ onMounted(filter);
 <template>
   <main class="container pt-5">
     <div class="row">
-      <h2>Browse BakRep Genomes</h2>
+      <h2>Search</h2>
     </div>
-    <div class="row">
-      <div class="col">
-        <div class="rounded bg-body-secondary p-4 mb-4">
-          <QueryFilter label="GC" v-model="gcTuple" />
-          <QueryFilter label="Contigs" v-model="contigTuple" />
-          <QueryFilter label="Genome Size" v-model="sizeTuple" />
-          <QueryFilter label="Completeness" v-model="qualityTuple" />
-          <QueryFilter label="Contamination" v-model="contaminationTuple" />
-          <button class="btn btn-light w-100" @click="filter()">
-            Apply Filter
-          </button>
+    <div class="container page-body flex-grow-1 ">
+      <form ref="submitform" @submit.prevent=submit()>
+        <div class="mb-3">
+          <textarea 
+          class="form-control form-control-lg" 
+          type="text"
+          v-model.trim="sequence"
+          id="searchfield"
+          name="searchfield"
+          placeholder="Paste your protein, sORF, ID or FASTA file here..."
+          aria-label="Paste your protein, sORF, ID or FASTA file here..."
+          autofocus="true"
+          rows="3"
+          minlength="7"
+          maxlength="303"
+          required="true"
+          onkeypress="return event.keyCode != 13;"></textarea>
+          <p>
+            Examples: MRTGNAN, ...
+          </p>
         </div>
-      </div>
+        <div class="input-group mb-3">
+          <input
+            class="form-control"
+            type="file"
+            id="fastaFile"
+            accept=".faa,.fas,.fna,.fasta,.faa.gz,.fna.gz,.fas.gz,.fasta.gz"
+          />
+        </div>
+        <h5 class="mb-2">Input type</h5>
+        <div class="mb-3">
+          <label class="form-check-label" for="inputTypeAuto">
+            <input class="form-check-input" type="radio" v-model="inputType" value="auto" name="inputType" id="inputTypeAuto" checked>
+            Auto
+          </label>
+          <label class="form-check-label" for="inputTypeProtein">
+            <input class="form-check-input" type="radio" v-model="inputType" value="protein" name="inputType" id="inputTypeProtein">
+            Protein
+          </label>
+          <label class="form-check-label" for="inputTypeDNA">
+            <input class="form-check-input" type="radio" v-model="inputType" value="dna" name="inputType" id="inputTypeDNA">
+            DNA
+          </label>
+          <label class="form-check-label" for="inputTypeId">
+            <input class="form-check-input" type="radio" v-model="inputType" value="id" name="inputType" id="inputTypeId">
+            ID
+          </label>
+        </div> 
+
+        <h5 class="mb-2">Search type</h5>
+        <div class="mb-3">
+          <label class="form-check-label" for="inputTypeAuto">
+            <input class="form-check-input" type="radio" v-model="searchType" value="auto" name="searchType" id="searchType" checked>
+            Exact
+          </label>
+          <label class="form-check-label" for="inputTypeProtein">
+            <input class="form-check-input" type="radio" v-model="searchType" value="protein" name="searchType" id="searchType">
+            BLAST
+          </label>
+        </div>
+
+        <h5 class="mb-2">BLAST search parameters</h5>
+        <div class="mb-3">
+          <label class="form-label" for="identity">
+            Identity
+            <input
+              class="form-control"
+              type="number"
+              min="30"
+              max="100"
+              v-model.number="identity"
+              id="identity"
+            />
+          </label>
+          <label class="form-label" for="coverage">
+            Coverage
+            <input
+              class="form-control"
+              type="number"
+              min="30"
+              max="100"
+              v-model.number="coverage"
+              id="coverage"
+            />
+          </label>
+        </div>
+
+        <div class="d-flex justify-content-beginning mb-5">
+            <button
+              v-if="!submitting"
+              class="btn btn-primary"
+              type="button"
+              id="submit-button"
+              :disabled="!valid"
+              @click="submit"
+            >
+              Search
+            </button>
+            <button
+              v-if="submitting"
+              class="btn btn-secondary"
+              type="button"
+              disabled
+            >
+              Searching...
+            </button>
+          </div>
+      </form>
+      <notification :message="error" />
     </div>
+
+    <!--TODO add tsv export-->
     <Loading :state="searchState">
       <div class="row px-3">
         Showing results {{ positionInResults.firstElement }}-{{
@@ -148,4 +335,9 @@ onMounted(filter);
     </Loading>
   </main>
 </template>
-<style></style>
+
+<style>
+  label + label {
+    margin-left: 10px;
+  }
+</style>
