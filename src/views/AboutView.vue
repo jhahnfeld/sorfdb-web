1260663efc7636b88cfa5bc2d45e543f4856685e
jhahnfel <julian.hahnfeld@computational.bio.uni-giessen.de>
2023-11-22 15:05:45 +0100

    Correct github link

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,15 +2,32 @@
   <div class="container">
     <h1>About sORFdb</h1>
     <p>
-        sORFdb - A database for sORFs, small proteins, and small protein groups in bacteria
+      sORFdb - A database for sORFs, small proteins, and small protein groups in
+      bacteria
     </p>
     <p>
-      This website provides access to the sORFdb database. All information that is given below refers to this web service. Information regarding the commandline tool are provided via <a href="https://github.com/oschwengers/bakta" target="_blank">GitHub</a>.
+      This website provides access to the sORFdb database. All information that
+      is given below refers to this web service. Information regarding the
+      commandline tool are provided via
+      <a href="https://github.com/jhahnfeld/sorfdb-web" target="_blank"
+        >GitHub</a
+      >.
     </p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, porro. Debitis numquam unde fugit ut, molestias cumque quibusdam error fugiat vitae. Enim quam pariatur deserunt molestias maxime autem voluptatum omnis?
-    Eius tenetur culpa tempore accusantium perspiciatis ullam voluptatibus vel facilis debitis alias quos dignissimos dolore, odio illum facere repellat nostrum id! Eius est maxime, eveniet aliquid alias accusantium ab dolorum?
-    Quisquam cupiditate, dolor quae fuga architecto similique expedita blanditiis tempore et. Dolorum minus ex recusandae dolor fugiat laborum praesentium eum, magnam earum ipsum harum beatae distinctio, voluptatem assumenda iusto sunt.
-    Distinctio ad iure eum officia sit sed, asperiores praesentium reprehenderit laudantium maiores tenetur recusandae, nemo sint! Voluptates ea enim accusamus vitae excepturi praesentium. Cupiditate facilis ducimus quo dolores laboriosam eum?
-    Non temporibus ex enim maxime molestias, quidem rem cupiditate nemo! A aspernatur ut, aut praesentium repellat cum id. Vero soluta velit sapiente aliquam quas repudiandae iure atque fugiat vel ad?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, porro.
+    Debitis numquam unde fugit ut, molestias cumque quibusdam error fugiat
+    vitae. Enim quam pariatur deserunt molestias maxime autem voluptatum omnis?
+    Eius tenetur culpa tempore accusantium perspiciatis ullam voluptatibus vel
+    facilis debitis alias quos dignissimos dolore, odio illum facere repellat
+    nostrum id! Eius est maxime, eveniet aliquid alias accusantium ab dolorum?
+    Quisquam cupiditate, dolor quae fuga architecto similique expedita
+    blanditiis tempore et. Dolorum minus ex recusandae dolor fugiat laborum
+    praesentium eum, magnam earum ipsum harum beatae distinctio, voluptatem
+    assumenda iusto sunt. Distinctio ad iure eum officia sit sed, asperiores
+    praesentium reprehenderit laudantium maiores tenetur recusandae, nemo sint!
+    Voluptates ea enim accusamus vitae excepturi praesentium. Cupiditate facilis
+    ducimus quo dolores laboriosam eum? Non temporibus ex enim maxime molestias,
+    quidem rem cupiditate nemo! A aspernatur ut, aut praesentium repellat cum
+    id. Vero soluta velit sapiente aliquam quas repudiandae iure atque fugiat
+    vel ad?
   </div>
 </template>
