893a606d85fccae07ad942bb2863d60d6f724eda
jhahnfel <julian.hahnfeld@computational.bio.uni-giessen.de>
2024-04-18 17:45:51 +0200

    Add Cluster Api

--- src/SorfdbApi.ts
+++ src/SorfdbApi.ts
@@ -11,6 +11,12 @@ import {
   type SorfdbSearchResult,
   type SorfdbEntry,
 } from "./model/SorfdbSearchResult";
+import {
+  SorfdbClusterSearchResultScheme,
+  ClusterEntryScheme,
+  type SorfdbClusterSearchResult,
+  type ClusterEntry,
+} from "./model/ClusterSearchResult";
 
 let baseurl: string = "http://localhost:8080";
 function initApi(url: string) {
@@ -90,9 +96,86 @@ class SorfdbApiImpl implements SorfdbApi {
   }
 }
 
+interface SorfdbClusterApi {
+  entry(id: string): Promise<ClusterEntry>;
+  search(request: SearchRequest): Promise<SorfdbClusterSearchResult>;
+  searchTsv(
+    request: SearchRequest,
+    includeHeader?: boolean,
+    abort?: AbortController | undefined,
+  ): Promise<TsvSearchResult>;
+  searchinfo(): Promise<SearchInfo>;
+}
+
+class SorfdbClusterApiImpl implements SorfdbClusterApi {
+  baseurl: string;
+  constructor(baseurl: string) {
+    this.baseurl = baseurl;
+  }
+
+  toJson(r: Response): Promise<any> {
+    if (!r.ok)
+      return r
+        .text()
+        .then((t) => Promise.reject(`${r.status}: ${r.statusText}\n${t}`));
+    return r.text().then(json5.parse);
+  }
+  entry(id: string): Promise<ClusterEntry> {
+    return fetch(baseurl + "/clusters/" + encodeURIComponent(id))
+      .then(this.toJson)
+      .then((j) => ClusterEntryScheme.parse(j));
+  }
+  searchinfo(): Promise<SearchInfo> {
+    return fetch(baseurl + "/clusters/search/_info")
+      .then(this.toJson)
+      .then((j) => j as SearchInfo);
+  }
+  search(request: SearchRequest): Promise<SorfdbClusterSearchResult> {
+    return fetch(baseurl + "/clusters/search", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(request),
+    })
+      .then(this.toJson)
+      .then((j) => SorfdbClusterSearchResultScheme.parse(j));
+  }
+  searchTsv(
+    request: SearchRequest,
+    includeHeader = true,
+    abort: AbortController | undefined = undefined,
+  ): Promise<TsvSearchResult> {
+    let path = baseurl + "/clusters/search";
+    if (!includeHeader) path += "?header=false";
+    const options: RequestInit = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "text/tab-separated-values",
+      },
+      body: JSON.stringify(request),
+    };
+    if (abort) options.signal = abort.signal;
+    return fetch(path, options)
+      .then((r) => {
+        if (!r.ok)
+          return r
+            .text()
+            .then((t) => Promise.reject(`${r.status}: ${r.statusText}\n${t}`));
+        return r.text();
+      })
+      .then(tsvSearchResultFromText);
+  }
+}
+
 function useApi(): SorfdbApi {
   return new SorfdbApiImpl(baseurl);
 }
 
-export { initApi, useApi };
-export type { SorfdbApi };
+function useClusterApi(): SorfdbClusterApi {
+  return new SorfdbClusterApiImpl(baseurl);
+}
+
+export { initApi, useApi, useClusterApi };
+export type { SorfdbApi, SorfdbClusterApi };
