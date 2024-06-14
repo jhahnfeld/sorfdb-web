fbd6ee38e0c9e4b31a5d41adb9bf5c779d941b77
jhahnfel <84308019+jhahnfeld@users.noreply.github.com>
2024-06-14 09:42:54 +0200

    Tmp remove support mail until ticket system is ready

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -14,12 +14,15 @@
       For technical questions and bug reports, we happily accept issues on
       <a href="https://github.com/jhahnfeld/sorfdb-web" target="_blank"
         >GitHub</a
-      >. <br />
+      >.
+    </p>
+    <!--
+     <br />
       General contact and questions:
       <a href="mailto:sorfdb@computational.bio.uni-giessen.de"
         >sorfdb@computational.bio.uni-giessen.de</a
       >
-    </p>
+    -->
     <p>
       sORFdb is a specialized (unlike GenBank, Swiss-Prot and UniProt), taxonomy
       independent (unlike SmProt) and quality-filtered (unlike GenBank and
